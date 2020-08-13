45e38c07231e5b1e5c56edfce5a40c6f798601a0
Jessica <duellje@oregonstate.edu>
2020-08-13 15:15:45 -0400

    initialized router

--- api/server.js
+++ api/server.js
@@ -2,10 +2,13 @@ const express = require("express");
 
 const server = express();
 
+const recipeRouter = require("../recipes/recipe-router");
+
 server.use(express.json());
+server.use("/api/recipes", recipeRouter);
 
 server.get("/", (req, res) => {
-  res.status(200).json({ api: "working" });
+  res.status(200).json({ server: "working" });
 });
 
 module.exports = server;
